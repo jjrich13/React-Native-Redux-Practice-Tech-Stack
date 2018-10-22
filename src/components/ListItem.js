3ed66d1d6a3c1829895db0e093c33b9d6819baf6
Jeff Richardson <jjrich13@gmail.com>
2018-10-22 15:46:27 -0500

    Added action creator, list of libraries displaying, clicking on one expands the view

---
+++ src/components/ListItem.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    renderDescription = () => {
+        
+        if(this.props.expanded){
+            return(
+                <Text>
+                    {this.props.library.item.description}
+                </Text>
+            )
+        }
+    }
+
+    render() {
+        const { id, title } = this.props.library.item
+        const { titleStyle } = styles
+
+        return (
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
+        )
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const expanded = ownProps.library.item.id === state.selectedLibraryId
+    return {
+        expanded
+    }
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
